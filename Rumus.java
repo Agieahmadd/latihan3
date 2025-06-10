6a99b7d40e9abbcf4ee627758a4731fdba5b39a2
Agieahmadd <agieaeae@gmail.com>
2025-06-10 13:38:29 +0800

    menambahkan atribut

--- Rumus.java
+++ Rumus.java
@@ -2,4 +2,10 @@ public class Rumus{
     //Ahmad Agie Munawar
     //2310010659
     //TI-4A
+
+    //atribut bertipe bilangan bulat
+    private int sisi;
+    private int panjang;
+    private int lebar;
+    private int hasil;
 }
