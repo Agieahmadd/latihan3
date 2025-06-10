b4d05861d267f8b3a114531a7c741f2751024bb9
Agieahmadd <agieaeae@gmail.com>
2025-06-10 13:41:33 +0800

    membuat mutator

--- Rumus.java
+++ Rumus.java
@@ -1,11 +1,21 @@
-public class Rumus{
-    //Ahmad Agie Munawar
-    //2310010659
-    //TI-4A
+public class Rumus {
+    // Ahmad Agie Munawar
+    // 2310010659
+    // TI-4A
 
-    //atribut bertipe bilangan bulat
+    // Atribut bertipe bilangan bulat
     private int sisi;
     private int panjang;
     private int lebar;
     private int hasil;
+
+    // Method untuk menghitung luas persegi (sisi * sisi)
+    public int hitungLuasPersegi(int sisi) {
+        return sisi * sisi;
+    }
+
+    // Method untuk menghitung luas persegi panjang (panjang * lebar)
+    public int hitungLuasPersegiPanjang(int panjang, int lebar) {
+        return panjang * lebar;
+    }
 }
