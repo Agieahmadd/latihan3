38d86bdc307d1aedf1e9f0444dd0c660ea22e3d4
Agieahmadd <agieaeae@gmail.com>
2025-06-10 13:56:15 +0800

    membuat class main

--- Rumus.java
+++ Rumus.java
@@ -23,14 +23,32 @@ public class Rumus {
     }
 
     // Method untuk menghitung luas persegi (sisi * sisi)
-    public int hitungLuasPersegi(int sisi) {
-        hasil = sisi * sisi;
-        return hasil;
+    public void hitungLuasPersegi(int sisi) {
+        this.hasil = sisi * sisi;
     }
 
     // Method untuk menghitung luas persegi panjang (panjang * lebar)
-    public int hitungLuasPersegiPanjang(int panjang, int lebar) {
-        hasil = panjang * lebar;
-        return hasil;
+    public void hitungLuasPersegiPanjang(int panjang, int lebar) {
+        this.hasil = panjang * lebar;
+    }
+}
+
+// class main
+public class RumusBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek Agie dari kelas Rumus
+        Rumus Agie = new Rumus();
+
+        // Menjalankan method untuk menghitung luas persegi
+        Agie.hitungLuasPersegi(Agie.sisi);
+
+        // Menampilkan hasil luas persegi
+        System.out.println("Luas Persegi: " + Agie.getHasil());
+
+        // Menjalankan method untuk menghitung luas persegi panjang
+        Agie.hitungLuasPersegiPanjang(Agie.panjang, Agie.lebar);
+
+        // Menampilkan hasil luas persegi panjang
+        System.out.println("Luas Persegi Panjang: " + Agie.getHasil());
     }
 }
